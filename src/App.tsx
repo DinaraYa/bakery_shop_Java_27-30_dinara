e9ef5475408c059778b7f7bc88c22b530531ea00
DinaraYa <tylemisovadinara@gmail.com>
2025-06-19 00:01:35 +0300

    component NavigatorDeskTop was created based on MUI <Tab>

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,24 @@ import Orders from "./components/Orders.tsx";
 import ShoppingCart from "./components/ShoppingCart.tsx";
 import Bread from "./components/Bread.tsx";
 import Dairy from "./components/Dairy.tsx";
-import Navigator from "./components/navigation/Navigator.tsx";
 import {navItems, productsItems} from "./configurations/nav-config.ts";
 import ErrorPage from "./components/servicePages/ErrorPage.tsx";
+import NavigatorDeskTop from "./components/navigation/NavigatorDeskTop.tsx";
 
 
 function App() {
     return (
         <Routes>
             {/*<Route path={Paths.HOME} element={<Layout/>}>*/}
-            <Route path={Paths.HOME} element={<Navigator items={navItems}/>}>
+            {/*<Route path={Paths.HOME} element={<Navigator items={navItems}/>}>*/}
+            <Route path={Paths.HOME} element={<NavigatorDeskTop items={navItems}/>}>
                 <Route index element={<Home/>}/>
                 <Route path={Paths.CUSTOMERS} element={<Customers/>}/>
                 <Route path={Paths.ORDERS} element={<Orders/>}/>
                 <Route path={Paths.CART} element={<ShoppingCart/>}/>
                 {/*<Route path={Paths.PRODUCTS} element={<Products/>}/>*/}
                 {/*<Route path={Paths.PRODUCTS} element={<ProductLayout/>}>*/}
-                    <Route path={Paths.PRODUCTS} element={<Navigator items={productsItems} sub={'sub'}/>}>
+                    <Route path={Paths.PRODUCTS} element={<NavigatorDeskTop items={productsItems}/>}>
                     <Route path={Paths.BREAD} element={<Bread/>}/>
                     <Route path={Paths.DAIRY} element={<Dairy/>}/>
                 </Route>
